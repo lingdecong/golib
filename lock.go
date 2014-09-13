d59cd870cf96aa73bf430085df9c64320287c15c
jamesdcl <lingdecong@gmail.com>
2014-09-13 10:45:46 +0800

    add nonblock function

--- lock.go
+++ lock.go
@@ -9,6 +9,10 @@ func LockFile(file *os.File) error {
 	return syscall.Flock(int(file.Fd()), syscall.LOCK_EX)
 }
 
+func LockFileNonBlock(file *os.File) error {
+	return syscall.Flock(int(file.Fd()), syscall.LOCK_EX|syscall.LOCK_NB)
+}
+
 func UnlockFile(file *os.File) error {
 	return syscall.Flock(int(file.Fd()), syscall.LOCK_UN)
 }
